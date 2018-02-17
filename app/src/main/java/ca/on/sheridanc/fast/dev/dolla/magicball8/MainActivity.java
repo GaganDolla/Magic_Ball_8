fcb5371fd1a54506a9657a6d2c2222d83333c2ea
Gagandeep Dolla <32396763+GaganDolla@users.noreply.github.com>
2018-02-17 11:34:58 -0500

    Initial commit

---
+++ app/src/main/java/ca/on/sheridanc/fast/dev/dolla/magicball8/MainActivity.java
@@ -0,0 +1,38 @@
+package ca.on.sheridanc.fast.dev.dolla.magicball8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button ask;
+        ask = (Button) findViewById(R.id.Ask);
+        final ImageView main = (ImageView) findViewById(R.id.Ball);
+        final int[] roll = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+        ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(4);
+                main.setImageResource(roll[number]);
+            }
+           });
+    }
+}
